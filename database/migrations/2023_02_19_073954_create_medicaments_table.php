4952cf1c3c71ea9e99168cff27aa4ad88901f886
rojonantenaina <rojonantenaina.nr@gmail.com>
2023-02-19 14:16:23 +0300

    first commit

---
+++ database/migrations/2023_02_19_073954_create_medicaments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicaments', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomMedicament');
+            $table->string('codeProduit');
+            $table->integer('quantiteMediament');
+            $table->float('coutUnitaire');
+            $table->float('prixVente');
+            $table->integer('nombrePlaquette');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicaments');
+    }
+}
